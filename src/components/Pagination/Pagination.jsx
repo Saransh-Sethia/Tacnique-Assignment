19004e0e1f8a6bc8c3e7ad05f677d226e3f6c367
saransh <sethia.saransh.ss@gmail.com>
2024-12-02 00:38:59 +0530

    fifth commit

--- src/components/Pagination/Pagination.jsx
+++ src/components/Pagination/Pagination.jsx
@@ -5,7 +5,7 @@ import { Box } from '@mui/material';
 const Pagination = ({previousPage,nextPage,currentPage,usersPerPage,totalUsers}) => {
     const pageNumbers = Math.ceil(totalUsers/usersPerPage);
   return (
-    <Box sx={{display:"flex", gap:"10px"}}>
+    <Box className="btn" sx={{display:"flex", gap:"10px"}} xs={12} sm={12} md={12} lg={1}>
       <button disabled={currentPage === 1 ? true : false} onClick={()=>previousPage()}>Previous</button>
       <button className='page-btn'>{currentPage}</button>
       <button disabled={currentPage === pageNumbers ? true : false} onClick={()=>nextPage(pageNumbers)}>Next</button>
